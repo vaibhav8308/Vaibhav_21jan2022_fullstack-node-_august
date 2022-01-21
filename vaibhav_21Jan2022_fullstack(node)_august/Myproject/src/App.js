7072a6861b0fa1a9bd5c58b478f9e51a431caeb8
vaibhav8308 <vaibhav.maske1997@gmail.com>
2022-01-21 20:02:42 +0530

    added sucessfully

---
+++ vaibhav_21Jan2022_fullstack(node)_august/Myproject/src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+<a href="/"> Checkout Page </a>
+      </header>
+      <main>Product Table</main>
+
+    </div>
+  );
+}
+
+export default App;
